c9846878f5ae4d0568c0b3116bc9917ae754a2fb
Ruchi Badkur <94377483+RuchiBadkur@users.noreply.github.com>
2024-02-20 15:58:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
-function getFormvalue(e) {
+function getFormvalue() {
     //Write your code here
-	e.preventDefault();
+	
 	const fname = document.querySelector('input[name="fname"]').value;
 	const lname = document.querySelector('input[name="lname"]').value;
 	alert(fname + " " + lname);
