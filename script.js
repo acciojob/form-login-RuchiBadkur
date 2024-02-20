56d19367ed0b2cd5f45325d8eb0a01394b7ae870
Ruchi Badkur <94377483+RuchiBadkur@users.noreply.github.com>
2024-02-20 12:52:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,8 @@
 function getFormvalue(e) {
     //Write your code here
 	e.preventDefault();
-	cont fname = document.querySelector("input[name='fname']").value;
-	cont lname = document.querySelector("input[name='lname']").value;
-	(!fname || !lname)
-		? alert("please enter both fields")
-		:alert(fname + " " + lname);
+	const fname = document.querySelector('input[name="fname"]').value;
+	const lname = document.querySelector('input[name="lname"]').value;
+	(fname || lname)
+		?alert(fname + " " + lname): ;
 }
